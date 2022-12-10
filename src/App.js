434d43f88be178ff228b1074ee2d89e24fe0e078
shraiyan47 <shraiyan47@gmail.com>
2022-12-11 02:47:33 +0600

    Profile Templet ready static

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import "./custom.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/fontawesome/css/all.min.css'
+import Profile from './Profile';
+// import Projects from './Projects';
 
 function App() {
   return (
-    <div >
-      <div className="header">
-        Hi, I am Shahadat Hossain 
-      </div>
-    </div>  
+    <div className="">
+      <section style={{ backgroundColor: "#eee" }} >
+        <div className="container py-5">
+
+          {/* Nav  */}
+          {/* <div className="row">
+            <div className="col">
+              <nav aria-label="breadcrumb" className="bg-light rounded-3 p-3 mb-4">
+                <ol className="breadcrumb mb-0">
+                  <li className="breadcrumb-item"><a href="/">Home</a></li> 
+                  <li className="breadcrumb-item active" aria-current="page">Shahadat Hossain's Profile</li>
+                </ol>
+              </nav>
+            </div>
+          </div> */}
+
+          <h3 className='text-center mt-2'>
+            Shahadat Hossain's Profile
+          </h3>
+          <hr />
+
+          {/* Profile Container  */}
+
+          <Profile />
+
+          {/* 
+          
+          <h3 className='text-center mt-5'>
+            Projects
+          </h3>
+          <hr /> 
+          
+          */}
+
+          {/* Project Container  */}
+
+          {/* <Projects /> */}
+
+        </div>
+      </section>
+    </div>
   );
 }
 
